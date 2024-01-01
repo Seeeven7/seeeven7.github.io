c1bb6a15711fde0d19f466279d7d16588ba9df1f
Christian Daus <christian.daus1@web.de>
2024-01-01 02:10:24 +0100

    image gallery changes

--- templates.js
+++ templates.js
@@ -1,11 +1,15 @@
 const menu = `
 <ul>
   <li><a class="nav-link" href="/">Home</a></li>
+
   <li>
     <a class="nav-link" href="/dev-projects">Dev Projects</a>
   </li>
+
   <li><a class="nav-link" href="/photography">Photography</a></li>
+  
   <li><a class="nav-link" href="/about">About</a></li>
+  
   <li><a class="nav-link" href="/contact">Contact</a></li>
 </ul>
 
