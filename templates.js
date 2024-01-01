0ee918985bde4e00c1443b1e463f45f6a562ace7
Christian Daus <christian.daus1@web.de>
2024-01-01 13:10:04 +0100

    gallery 1.0

--- templates.js
+++ templates.js
@@ -1,11 +1,11 @@
 const menu = `
 <ul>
   <li><a class="nav-link" href="/">Home</a></li>
-
+  
   <li>
     <a class="nav-link" href="/dev-projects">Dev Projects</a>
   </li>
-
+  
   <li><a class="nav-link" href="/photography">Photography</a></li>
   
   <li><a class="nav-link" href="/about">About</a></li>
@@ -17,5 +17,8 @@ const menu = `
 `;
 
 const footer = `
+  <hr aria-hidden="true" role="separator" />
   <p>Powered by Web Standards</p>
+  <p><s>&copy;</s> No Copyright, feel free to take what you need.</br>
+  The pictures are mine though</p>
 `;
