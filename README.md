5ea20bbd12573d2bcea358a74eeeb031af5616a6
Christian Daus <christian.daus1@gmail.com>
2024-01-03 17:22:28 +0100

    not needed

---
+++ README.md
@@ -0,0 +1,11 @@
+## About the Project
+
+This is my personal Developer Portfolio Website.
+
+### Built With
+
+Vanilla / Pure
+
+- JavaScript
+- HTML
+- CSS
---
+++ templates.js
@@ -0,0 +1,17 @@
+const menu = `
+<ul>
+  <li><a class="nav-link" href="/">Home</a></li>
+  <li>
+    <a class="nav-link" href="/dev-projects">Dev Projects</a>
+  </li>
+  <li><a class="nav-link" href="/photography">Photography</a></li>
+  <li><a class="nav-link" href="/about">About</a></li>
+  <li><a class="nav-link" href="/contact">Contact</a></li>
+</ul>
+
+<hr aria-hidden="true" role="separator" />
+`;
+
+const footer = `
+  <p>Powered by Web Standards</p>
+`;
