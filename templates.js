b2b5bc866f02c21b4aa77fad3d3229d7a986f940
Christian Daus <christian.daus1@gmail.com>
2024-06-14 14:37:54 +0200

    refactor and content

--- templates.js
+++ templates.js
@@ -1,19 +1,17 @@
 const menu = `
+<div><a class="nav-link" href="/">Home</a></div>
+
 <ul>
-  <li><a class="nav-link" href="/">Home</a></li>
-  
   <li>
     <a class="nav-link" href="/dev-projects">Dev Projects</a>
   </li>
-  
+
   <li><a class="nav-link" href="/photo-gallery">Photo Gallery</a></li>
-  
+
   <li><a class="nav-link" href="/about">About</a></li>
-  
+
   <li><a class="nav-link" href="/contact">Contact</a></li>
 </ul>
-
-<hr aria-hidden="true" role="separator" />
 `;
 
 const footer = `
